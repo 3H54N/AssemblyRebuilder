58375b1dbaeeff55f5cb5141fd5770a01dced9ca
文煌 <wwh1004@hotmail.com>
2018-07-16 13:18:32 +0800

    添加MetaDataFlags.KeepOldMaxStack

--- AssemblyRebuilder/Properties/AssemblyInfo.cs
+++ AssemblyRebuilder/Properties/AssemblyInfo.cs
@@ -1,33 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
 [assembly: AssemblyTitle("AssemblyRebuilder")]
 [assembly: AssemblyDescription("重建程序集")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AssemblyRebuilder")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-//将 ComVisible 设置为 false 将使此程序集中的类型
-//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
-//请将此类型的 ComVisible 特性设置为 true。
+[assembly: AssemblyCopyright("Copyright © 2018 Wwh / Net7Cracker")]
 [assembly: ComVisible(false)]
-
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("b54f91e5-a01b-473b-b3ea-570c6617f2df")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.2.1.1")]
+[assembly: AssemblyFileVersion("1.2.1.1")]
